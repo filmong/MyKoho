96c868799d18c9fd9bf874a4389947d1f06528af
Filmon Ghebre <fiha00@gmail.com>
2018-12-10 10:22:15 -0800

    Update HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,1 +1,12 @@
+using System;
 
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
